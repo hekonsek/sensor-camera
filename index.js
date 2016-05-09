e67d34d83d37cd0b568c0e1a5a8a684bd83d8f68
Henryk Konsek <hekonsek@gmail.com>
2016-05-09 12:53:53 +0200

    Added images directory creation.

--- index.js
+++ index.js
@@ -4,31 +4,33 @@ var mkdirp = require('mkdirp');
 var imagesDirectory = '/var/rhiot/camera/queue'
 
 mkdirp(imagesDirectory, function (err) {
+
     if (err) console.error(err);
-});
 
-var camera = new RaspiCam({
-	mode: "timelapse",
-	output: imagesDirectory + "/image_%06d.jpg",
-	encoding: "jpg",
-	timelapse: 1000,
-	timeout: 120000 // take a total of 4 pictures over 12 seconds
-});
+    var camera = new RaspiCam({
+        mode: "timelapse",
+        output: imagesDirectory + "/image_%06d.jpg",
+        encoding: "jpg",
+        timelapse: 1000,
+        timeout: 120000 // take a total of 4 pictures over 12 seconds
+    });
 
-camera.on("start", function( err, timestamp ){
-	console.log("timelapse started at " + timestamp);
-});
+    camera.on("start", function( err, timestamp ){
+        console.log("timelapse started at " + timestamp);
+    });
 
-camera.on("read", function( err, timestamp, filename ){
-	console.log("timelapse image captured with filename: " + filename);
-});
+    camera.on("read", function( err, timestamp, filename ){
+        console.log("timelapse image captured with filename: " + filename);
+    });
 
-camera.on("exit", function( timestamp ){
-	console.log("timelapse child process has exited");
-});
+    camera.on("exit", function( timestamp ){
+        console.log("timelapse child process has exited");
+    });
 
-camera.on("stop", function( err, timestamp ){
-	console.log("timelapse child process has been stopped at " + timestamp);
-});
+    camera.on("stop", function( err, timestamp ){
+        console.log("timelapse child process has been stopped at " + timestamp);
+    });
 
-camera.start();
+    camera.start();
+
+});
